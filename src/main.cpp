c9c35d6e8cf272a0998ffe80f06883ef03aad931
cosmoslx <cosmoslx@doctor-li.cn>
2023-01-09 15:46:18 +0800

    feat: tuple and array basic usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <functional>
+#include <tuple>
+#include <array>
 
 using Notify = void(int, int);
 using pNotify = void(*)(int, int);
@@ -13,6 +15,10 @@ void notification(int x, int y) {
     std::cout << "x*y: " << x*y << std::endl;
 }
 
+constexpr int fibonacci(const int n) {
+    return n == 1 || n == 2 ? 1 : fibonacci(n-1) + fibonacci(n-2);
+}
+
 int main(void) {
 
     Notify* notify = [](int x, int y) {
@@ -24,6 +30,17 @@ int main(void) {
     send_notification(notification);
 
     std::cout << "Hello, C++" << std::endl;
+    std::cout << "fibonacci(10): " << fibonacci(10) << std::endl;
+
+    auto [x, y, z] = std::make_tuple(1, 2.3, "456");
+    std::cout << "x=" << x << ", y=" << y << ", z=" << z << std::endl;
+
+    auto arr = std::array<int, 5>{1, 2, 3, 4, 5};
+    std::cout << "arr:" << std::endl;
+    for (auto& a : arr) {
+        std::cout << a << std::endl;
+    }
+
 
     return 0;
 }
